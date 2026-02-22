46fec7c6ccc18fcadf30b5d506b7272d05239357
Nguyễn Thế Ngọc Hà <88977475+Strkss@users.noreply.github.com>
2026-02-22 16:55:56 +0700

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    java
+    id("org.springframework.boot") version "4.0.3"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "org.strycks"
+version = "0.0.1-SNAPSHOT"
+description = "wishlist"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-ldap")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.mysql:mysql-connector-j")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-ldap-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    runtimeOnly("com.h2database:h2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
